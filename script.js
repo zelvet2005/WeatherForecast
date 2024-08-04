f2a6e5071cdf6cc509e5db99b9362a877e080675
Vetal <vitalikzeleneckij3@gmail.com>
2024-08-04 21:26:17 +0300

    added flowchart

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 "use strict";
+
+// const apiKey = "0ddba79c202945448d9175312240308";
+// const q = "Gaisin"; // temporary, required implement through location
+// const days = 7;
+
+// fetch(
+//   `http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${q}&days=${days}`
+// )
+//   .then((response) => response.json())
+//   .then((data) => console.log(data));
