5bd04b662224ec0d631dc9ce291e5f8aedbcc0ff
Vetal <vitalikzeleneckij3@gmail.com>
2024-09-20 18:43:51 +0300

    added structure of files

--- code/weather.js
+++ code/weather.js
@@ -143,3 +143,47 @@ class WeatherApp {
 }
 
 const app = new WeatherApp();
+
+// const map = L.map("map").setView([50, 30], 6);
+
+// L.tileLayer(
+//   "https://tiles.stadiamaps.com/tiles/alidade_satellite/{z}/{x}/{y}{r}.{ext}",
+//   {
+//     minZoom: 0,
+//     maxZoom: 20,
+//     attribution:
+//       '&copy; CNES, Distribution Airbus DS, © Airbus DS, © PlanetObserver (Contains Copernicus Data) | &copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+//     ext: "jpg",
+//   }
+// ).addTo(map);
+
+// L.marker([50, 30]).addTo(map).openPopup();
+
+// const apiKey = "ee684e18c13cf1eae5220ab06846dfbb"; // OpenWeatherMap API key
+
+// const temperatureLayer = L.tileLayer(
+//   `https://tile.openweathermap.org/map/temp_new/{z}/{x}/{y}.png?appid=${apiKey}`,
+//   {
+//     attribution:
+//       '&copy; <a href="https://openweathermap.org/">OpenWeatherMap</a>',
+//     opacity: 1,
+//   }
+// );
+
+// const cloudsLayer = L.tileLayer(
+//   `https://tile.openweathermap.org/map/clouds_new/{z}/{x}/{y}.png?appid=${apiKey}`,
+//   {
+//     attribution:
+//       '&copy; <a href="https://openweathermap.org/">OpenWeatherMap</a>',
+//     opacity: 1,
+//   }
+// );
+
+// const precipitationLayer = L.tileLayer(
+//   `https://tile.openweathermap.org/map/precipitation_new/{z}/{x}/{y}.png?appid=${apiKey}`,
+//   {
+//     attribution:
+//       '&copy; <a href="https://openweathermap.org/">OpenWeatherMap</a>',
+//     opacity: 1,
+//   }
+// );
